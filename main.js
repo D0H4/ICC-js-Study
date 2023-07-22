acb2d8e74466fb8b5eab86f30649ef1c72fba6ee
xDohax <LeeDoha@proton.me>
2023-07-23 01:18:53 +0900

    week3

--- main.js
+++ main.js
@@ -1,23 +1,11 @@
-import _ from "lodash"
-
-const obj = {
-	a: 1,
-	b: 2,
-	c: 3,
-	//x: 6
+//일반 함수의 this는 호출 위치에서 정의
+const timer = {
+	title: 'timer',
+	timeout() {
+	console.log(this.title)
+	setTimeout (function () {
+		console.log(this.title)
+		}, 1000)
+	}
 }
-
-const {a, b} = obj;
-console.log(a,b)
-
-const {c} = obj;
-console.log(c)
-
-const {x = 4} =obj;
-console.log(x)
-
-const {c: test} = obj; // c를 할당
-console.log(test)
-
-const {c, ...rest} = obj; // c 제외
-console.log(c, rest)
+timer.timeout()
