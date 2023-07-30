5fb9789bfd8dea4302a0bb17669a62c025ae1002
xDohax <LeeDoha@proton.me>
2023-07-30 15:42:16 +0900

    week4

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-import _ from "lodash"
+class Kpop {
+    constructor(singerName, releaseDate, members) {
+        this.singerName = singerName
+        this.releaseDate = releaseDate
+        this.members = members
+    }
+    set reset(value) {
+        [this.singerName, this.releaseDate, this.members] = value.split(' ')
+    }
+}
 
-console.log('hello')
+class SummerComes extends Kpop {
+    constructor(leaderName, singerName, releaseDate, members) {
+        super(singerName, releaseDate, members)
+        this.leaderName = leaderName;
+    }
+    sing() {
+        return console.log("귀를 귀울여 듣고 싶은 맘이 설레이잖아")
+    }
+}
+
+const summerComes = new SummerComes('Hyojung')
+summerComes.sing()
+summerComes.reset = "OHMYGIRL 2023-07-24 6"
+
+console.log(summerComes)
