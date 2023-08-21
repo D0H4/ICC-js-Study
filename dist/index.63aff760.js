353489b0b26e7dd25e78177f2ad5ca94e9b0668f
xDohax <LeeDoha@proton.me>
2023-08-21 14:21:42 +0900

    week8 숙제

--- dist/index.63aff760.js
+++ dist/index.63aff760.js
@@ -574,40 +574,23 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"adjPd":[function(require,module,exports) {
-const users = [
-    {
-        name: "윤병현",
-        age: 24,
-        class: "1반"
-    },
-    {
-        name: "문채현",
-        age: 22,
-        class: "1반"
-    },
-    {
-        name: "최진영",
-        age: 21,
-        class: "2반"
-    },
-    {
-        name: "이윤수",
-        age: 20,
-        class: "2반"
+const getMovies = (movieName)=>{
+    return new Promise((resolve, reject)=>{
+        fetch(`http://www.omdbapi.com/?apikey=7035c60c&s=${movieName}`).then((res)=>res.json()).then((res)=>{
+            console.log(res);
+            if (res.Response === "False") reject(res.Error);
+            resolve(res);
+        });
+    });
+};
+const wrap = async ()=>{
+    try {
+        await getMovies("avengers");
+    } catch (error) {
+        console.log("에러 발생: ", error);
     }
-];
-const userData = users.filter((users)=>{
-    return users.age > 21 && users.class === "1반";
-});
-console.log(userData);
-const userData1 = users.map((user)=>{
-    return {
-        ...user,
-        email: "1234@naver.com",
-        tel: "010-1234-5678"
-    };
-});
-console.log(userData1);
+};
+wrap();
 
 },{}]},["lmKb7","adjPd"], "adjPd", "parcelRequire0ac8")
 
--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-const users = [
-	{name: '윤병현', age: 24, class: '1반'},
-	{name: '문채현',  age: 22, class: '1반'},
-	{name: '최진영',  age: 21, class: '2반'},
-  	{name: '이윤수', age: 20, class: '2반'}
-]
+const getMovies = movieName =>{
+	return new Promise((resolve, reject) => {
+		fetch(`http://www.omdbapi.com/?apikey=7035c60c&s=${movieName}`)
+			.then(res => res.json())
+			.then(res => {
+				console.log(res)
+				if(res.Response === 'False'){
+					reject(res.Error)
+				}
+				resolve(res)
+	})
+	})
+}
 
-const userData = users.filter(users => {
-	return users.age > 21 && users.class === "1반"
-})
+const wrap = async () => {
+  try {
+    await getMovies('avengers')
+  }
+  catch(error) {
+    console.log("에러 발생: ", error)
+  }
+}
 
-console.log(userData)
-
-const userData1 = users.map(user => {
-	return {
-		...user,
-		email: '1234@naver.com',
-		tel: '010-1234-5678'
-	}
-})
-
-console.log(userData1)
+wrap()
